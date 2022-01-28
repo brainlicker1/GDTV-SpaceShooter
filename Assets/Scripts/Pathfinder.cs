91fac64876ad7f31a9a8cc0bdea5ed78662930fe
DESKTOP-C61PO9C\Randomality <brownron199@yahoo.com>
2022-01-28 11:36:50 -0500

    Brain is mush, idk what i did, but it woks so thing stuff yadda yadda..

--- Assets/Scripts/Pathfinder.cs
+++ Assets/Scripts/Pathfinder.cs
@@ -4,11 +4,17 @@ using UnityEngine;
 
 public class Pathfinder : MonoBehaviour
 {
-    [SerializeField] WaveConfigSO waveConfig;
+    EnemySpawner enemySpawner;
+     WaveConfigSO waveConfig;
     List<Transform> waypoints;
     int waypointIndex = 0;
-    void Start()
+    void Awake()
     {
+        enemySpawner = FindObjectOfType<EnemySpawner>();
+    }
+    void Start()
+    {   
+        waveConfig = enemySpawner.GetCurrentWave();
         waypoints = waveConfig.GetWaypoints();
         transform.position = waypoints[waypointIndex].position;
         
